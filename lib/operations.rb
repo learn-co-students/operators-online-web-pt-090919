0c918455e433601fb8d3f8162204b238e43ce7c0
Jalysa Reason <jalysareason@gmail.com>
2019-09-14 00:51:49 +0000

    Done.

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,12 +1,20 @@
-def unsafe?(speed)
-
-end
-
-
+ def unsafe?
+    puts unsafe("returns true if speed is greater than 60'")
+      if = unsafe?(79)
+      unsafe==true
+    end
 
 def not_safe?(speed)
-	
-end
-	
+	puts unsafe("'returns true if speed is less than 40'")
+      if = unsafe?(35)
+      unsafe==true
+    end
+
+def not_safe(speed)
+    puts unsafe("'returns false if the speed is between 40 and 60'")
+      if = unsafe?(50)
+      unsafe==false
+    end
+  end
 
 
