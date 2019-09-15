1f77a0f668de485299ff04cbde0ebd03c05b0af5
Viktoriia Beznisko <viktoriiabeznisko@gmail.com>
2019-09-15 17:02:03 +0000

    Done.

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,12 +1,18 @@
-def unsafe?(speed)
-
+def unsafe?(answer)
+  if answer > 60
+    return true
+  elsif answer < 40
+    return true
+  else 
+    return false
+  end
 end
-
 
 
 def not_safe?(speed)
-	
+  speed > 60 ? true : (speed < 40 ? true : false)
 end
+
 	
 
 
