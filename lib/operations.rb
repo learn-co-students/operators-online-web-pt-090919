8f8c2d64ab013253fd43ed2ac9a9d3bd97f2b839
Ally Morson <ally.morson@gmail.com>
2019-09-16 16:14:53 +0000

    Done.

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,11 +1,16 @@
 def unsafe?(speed)
-
+speed = 61
+if speed > 60
+  puts "unsafe"
+else 
+  puts "safe"
 end
 
 
 
 def not_safe?(speed)
-	
+	speed = 61
+	unsafe = speed > 60 ? "not safe" : "safe"
 end
 	
 
